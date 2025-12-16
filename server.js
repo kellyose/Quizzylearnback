294dd124e2084c1f2555b3a3692d9da7c6f93a72
kellyose <171274602+kellyose@users.noreply.github.com>
2025-12-16 10:59:47 +0100

    added url

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://quizzylearn.onrender.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
